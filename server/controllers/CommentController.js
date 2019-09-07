5c805365dcd30af52539b12eae8f9cec578723c2
Mark Davich <markdavich@gmail.com>
2019-09-07 13:26:27 -0600

    Added a bunch of server status messages

--- server/controllers/CommentController.js
+++ server/controllers/CommentController.js
@@ -25,6 +25,7 @@ export default class CommentController{
       .post('', this.createNoBlogId)
       // http://localhost:3000/api/comments/5d73d6dddc8c5b55fbab123b
       .post('/:id', this.create)
+      .put('/:id', this.edit)
       
   }
 
@@ -69,6 +70,66 @@ export default class CommentController{
       response.send(newComment)
     } catch (error) {
       console.log('ERROR: CommentController.createNoBlogId()')
+      next(error)
+    }
+  }
+
+  async edit(reqest, response, next) {
+    let authorId = reqest.body.authorId
+
+    if (!authorId) {
+      return response
+        .status(400)
+        .send(`
+          Blogger Node Server: CommentController.edit()
+          authorId was not attached to request.body
+
+          400 Bad Request
+          The server cannot or will not process the request
+          due to an apparent client error
+          (e.g., 
+            malformed request syntax,
+            size too large, invalid request message framing,
+            or deceptive request routing
+          ).
+        `)
+    }
+
+    try {
+      let comment = await _commentService.findOneAndUpdate(
+        {
+          _id: reqest.params.id,
+          authorId: reqest.session.uid
+        },
+        reqest.body,
+        {
+          new: true
+        }
+      )
+
+      if (comment) {
+        // 200 OK
+        // Standard response for successful HTTP requests.
+        // The actual response will depend on the request method used.
+        // In a GET request, the response will contain an entity 
+        // corresponding to the requested resource. In a POST request,
+        // the response will contain an entity describing or containing
+        // the result of the action.
+        return response.status(200).send(comment)
+      } else {
+        return response
+          .status(204)
+          .send(`
+          Blogger Node Server: CommentController.edit()
+          request.body._id
+          OR
+          request.body.authorId
+          NOT FOUND
+
+          204: The server successfully processed the request and is not returning any content.
+        `)
+      }
+    } catch (error) {
       next(error)
     }
   }
